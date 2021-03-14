56f602f07cfe490708cb0eb22dd4f4f1b08ae67d
Anton Babushking <napishi_antonu@mail.ru>
2021-03-14 11:41:54 +0300

    add CreateRole test

--- src/test/java/AdminRegistration.java
+++ src/test/java/AdminRegistration.java
@@ -27,8 +27,29 @@ public class AdminRegistration {
         $("#loginField-1014-inputEl").setValue(this.passwordAdmin);
         $("#loginField-1015-inputEl").setValue(this.phoneNumberAdmin);
         $("#button-1026-tooltipEl").parent().click();
-        $("#button-1026").click();
-        $("html").shouldHave(Condition.text("Агент:Admin, номер:0000"));
+        $("#panel-1045-innerCt").shouldHave(Condition.text("Агент:Admin, номер:0000"));
+    }
+
+    @Test
+    void CreateRole() {
+        GoToListOfRoles();
+        CreateRole();
+    }
+
+    void GoToListOfRoles() {
+        // Перейти в раздел Настройки-Список ролей
+        $("#ext-element-164").click();
+        $("#ext-element-149").click();
+
+    }
+
+    void CreateRole() {
+        // Создать роль
+
+        $("#button-1429-btnInnerEl").click();
+        $("#textfield-2133-inputEl").setValue("Автороль");
+        $("#textareafield-2135-inputEl").setValue("это автотест");
+        $("#panel-1045-innerCt").shouldHave(Condition.text("Агент:Admin, номер:0000"));
     }
 }
 
